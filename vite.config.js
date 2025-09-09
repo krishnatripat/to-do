5006a9bc44f915873730ae8aa88c554d92d882d8
krishna tripathi <154440810+krishnatripat@users.noreply.github.com>
2025-09-09 16:44:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import react from '@vitejs/plugin-react'
 
 import { defineConfig } from 'vite'
-import fs from 'fs'
 
 export default defineConfig({
   server: {
     host: true,
-    port: 5173,
-    https: {
-      key: fs.readFileSync('./certs/key.pem'),
-      cert: fs.readFileSync('./certs/cert.pem')
-    }
+    port: 5173
   }
 })
 
